077dbb7800ebb8613d1a364d76eaee13254d6b37
Håkon Nessjøen <haakon@trippelm.no>
2019-06-10 17:39:01 +0200

    First import

---
+++ src/TCA6416A.cpp
@@ -0,0 +1,102 @@
+// TCA6416A module
+// Copyright 2019 Håkon Nessjøen
+#include <Arduino.h>
+#include "TCA6416A.h"
+
+// NB!: Only address 0 or 1
+void TCA6416A::begin(uint8_t address) {
+	i2caddr = 0x20 | address;
+	TW.begin();
+
+	port_read();
+	mode_read();
+}
+
+void TCA6416A::pin_write(uint8_t pinNum, uint8_t level) {
+	uint16_t mask = 1 << pinNum;
+
+	if (level == HIGH) {
+		pinState |= mask;
+	} else {
+		pinState &= ~mask;
+	}
+
+	port_write(pinState);
+}
+
+void TCA6416A::pin_mode(uint8_t pinNum, int mode) {
+	uint16_t mask = 1 << pinNum;
+
+	if (mode == INPUT) {
+		pinModes |= mask;
+	} else {
+		pinModes &= ~mask;
+	}
+
+	mode_write(pinModes);
+}
+
+int TCA6416A::pin_read(uint8_t pinNum) {
+	uint16_t mask = 1 << pinNum;
+
+	port_read();
+
+	if ((pinState & mask) == mask) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void TCA6416A::port_write(uint16_t i2cportval) {
+	TW.beginTransmission((int)i2caddr);
+	TW.write(TCAREG_OUTPUT0);
+
+	TW.write(i2cportval & 0x00FF);
+	TW.write(i2cportval >> 8 );
+
+	TW.endTransmission();
+
+	pinState = i2cportval;
+}
+
+uint16_t TCA6416A::port_read() {
+	TW.beginTransmission((int)i2caddr);
+	TW.write(TCAREG_INPUT0);
+	TW.endTransmission();
+
+	TW.requestFrom((int)i2caddr, (int)i2cwidth);
+
+	pinState = TW.read();
+	pinState |= TW.read() << 8;
+
+	return pinState;
+}
+
+void TCA6416A::mode_write(uint16_t modes) {
+	Serial.print("Mode Write ");
+	Serial.println(modes, HEX);
+
+	TW.beginTransmission((int)i2caddr);
+	TW.write(TCAREG_CONFIG0);
+
+	TW.write(modes & 0x00FF);
+	TW.write(modes >> 8 );
+
+	TW.endTransmission();
+
+	pinModes = modes;
+}
+
+uint16_t TCA6416A::mode_read() {
+	TW.beginTransmission((int)i2caddr);
+	TW.write(TCAREG_CONFIG0);
+	TW.endTransmission();
+
+	TW.requestFrom((int)i2caddr, (int)i2cwidth);
+
+	pinModes = TW.read();
+	pinModes |= TW.read() << 8;
+
+	return pinModes;
+}
---
+++ src/TCA6416A.h
@@ -0,0 +1,43 @@
+// TCA6416A module
+// Copyright 2019 Håkon Nessjøen
+#ifndef TCA6416A_H
+#define TCA6416A_H
+
+#include "Wire.h"
+
+#define TCAREG_INPUT0 0x00
+#define TCAREG_INPUT1 0x01
+#define TCAREG_OUTPUT0 0x02
+#define TCAREG_OUTPUT1 0x03
+#define TCAREG_POLARITY0 0x04
+#define TCAREG_POLARITY1 0x05
+#define TCAREG_CONFIG0 0x06
+#define TCAREG_CONFIG1 0x07
+
+/*
+ TODO: If I understood the manual correctly, the command only needs to be sent to change active register
+ so for performance I want to store the last used register, and if you are for example reading multiple times,
+ you would not need to resend the command the second time to read two more bytes. And the same way about writing.
+ The registers are paired, so each byte writes to register (REGISTER | (i % 2)) indefinitely.
+ */
+
+class TCA6416A {
+public:
+	void begin(uint8_t address);
+	void pin_mode(uint8_t pinNum, int mode);
+	void pin_write(uint8_t pinNum, uint8_t level);
+	int  pin_read(uint8_t pinNum);
+	void port_write(uint16_t i2cportval);
+	uint16_t port_read();
+	void mode_write(uint16_t i2cportval);
+	uint16_t mode_read();
+
+private:
+		TwoWire TW;
+    uint8_t i2caddr;
+    uint8_t i2cwidth;
+		uint16_t pinState;
+		uint16_t pinModes;
+};
+
+#endif // TCA6416A_H
