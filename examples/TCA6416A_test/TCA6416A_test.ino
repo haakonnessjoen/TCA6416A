59ebacc4beac0d1802dcfeaeb278083fe0ac17ea
Håkon Nessjøen <haakon@trippelm.no>
2019-06-14 13:56:19 +0200

    Updated example

--- examples/TCA6416A_test/TCA6416A_test.ino
+++ examples/TCA6416A_test/TCA6416A_test.ino
@@ -3,6 +3,8 @@
 TCA6416A pins;
 
 void setup() {
+	pins.begin(0); // Address 0 or 1, depending on your addr-pin
+
 	pins.pin_mode(0, OUTPUT);
 	pins.pin_mode(1, INPUT);
 }
@@ -13,6 +15,6 @@ void loop() {
 	pins.pin_write(0, LOW);
 	delay(200;
 
-	// If pin 1 is high, stop the loop
+	// If pin 1 (counted from 0) is high, stop the loop
 	while (pins.pin_read(1) == HIGH);
 }
